495fd300ed1bc76db3d690af2e427e8b7cab0566
Lizandra de Jesus Ferrari <lizandra.ferrari@hotmail.com>
2024-05-14 16:10:51 -0300

    index atualizado

--- main.js
+++ main.js
@@ -1,58 +1,56 @@
-function Logar(){
-    
-    let email = document.querySelector("#email").value;
-    let senha = document.querySelector("#senha").value;
-    let banco = JSON.parse(localStorage.getItem("banco")); 
-    
-    let UsuarioEncontrado = false;
-    
-    if(banco){
-        for (let v = 0 ; v < banco.length; v++){
-                if (email === banco[v].email && senha === banco[v].senha) {
-                    alert("Achou!");
-                    UsuarioEncontrado = true;
-                    break;
-                }
-            }
-            
-            if (UsuarioEncontrado) {
-                window.open("index.html");
-            }else{
-                alert("Cadastre-se!");
-                window.open("cadastro.html");
-            }
+//
+const express = require('express');
+const { createConnection } = require('net');
+const path = require('path');
+const app = express();
+const port = 6065;
+const dao = require('./database/dao')
+app.use(express.urlencoded({extended: true}))
+// Define uma rota para a página inicial
+app.get('/', (req, res) => {
+    res.json({ 'Status': 'OK' });
+});
+
+// Define a pasta onde os arquivos estáticos estão localizados
+const pastaEstática = path.join(__dirname, 'public');
+
+// Define a rota para servir arquivos estáticos a partir da pasta definida
+app.use(express.static(pastaEstática));
+
+// Define uma rota para a página index.html
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(pastaEstática, 'index.html'));
+});
+
+app.get('/login', (req,res) => res.sendFile(path.join(pastaEstática, 'login.html')))
+
+//Rota para o cadastro
+app.get('/cadastro', (req,res) => {
+    res.sendFile(path.join(pastaEstática, 'cadastro.html'))
+})
+
+//Rota para login
+
+//puxa da pag cadastro
+app.post('/cadastro' , (req , res) =>{    
+    let senhaVar1 = req.body.senhaInput1
+    let senhaVar2 = req.body.senhaInput2
+    if (senhaVar1 == senhaVar2){
+        const dados = {               //faz a requisição, do corpo, do item com atributo "name" req.body.name
+            login: req.body.nomeInput , email: req.body.emailInput , senha: senhaVar1
         }
- }
+        dao.addUser(dados , (result , err)=>{
+        }) 
+        //res.sendFile(path.join(pastaEstática , 'login.html')) 
+        res.redirect('/login')
+}else{
+    res.send('As senhas precisam ser iguais!!')
+}
+})
+// Inicia o servidor na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
 
-function Cadastrar(){
-    
-    let nome = document.querySelector("#nome").value;
-    let senha = document.querySelector("#senha1").value;
-    let senha2 = document.querySelector("#senha2").value;
-    let email = document.querySelector("#email").value;
-    let cep = document.querySelector("#cep").value;
-    let rua = document.querySelector("#rua").value;
-    let numero = document.querySelector("#numero").value;
-    let cidade = document.querySelector("#cidade").value;
-    let estado = document.querySelector("#estado").value;
-    let complemento = document.querySelector("#complemento").value;
+//nome:nome,senha:senha,email:email,estado:estado,cidade:cidade,cep:cep,rua:rua,complemento:complemento,numero:numero
 
-    const banco = JSON.parse(localStorage.getItem("banco")) || []
-    let provaSenha = false;
-
-    if (senha == senha2 ) {
-        provaSenha = true;
-    }else{
-        alert("Coloque sua senha igual a primeira!")
-        return;
-    }
-    if (provaSenha) {
-        let novoUsuario = {nome:nome,senha:senha,email:email,estado:estado,cidade:cidade,cep:cep,rua:rua,complemento:complemento,numero:numero}
-        banco.push(novoUsuario);
-        localStorage.setItem("banco",JSON.stringify(banco));
-        alert("cadastrado!");
-        window.open("login.html");
-    }else{
-        alert("Cadastro incompleto!");
-        }
-    }
--- public/components/header.js
+++ public/components/header.js
@@ -4,15 +4,20 @@ class Header extends HTMLElement {
     }
   
     connectedCallback() {
-      this.innerHTML = `
+      this.innerHTML = `      
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <link type="text/css" rel = "stylesheet" href="layout.css">
       <header>
-      <nav class="navbar navbar-dark bg-dark fixed-top">
+      <link rel="icon" href="image/apple.png">
+            <nav class="navbar navbar-dark bg-dark fixed-top">
       <div class="container-fluid">
         <a class="navbar-brand" href="index.html" >
         <img src = "image/iluga.png" style="width: 12%">
         </a>
+        <div id = "login" >
         <a class = "navbar-item linkdiv" href="cadastro.html">Cadastro</a>
         <a class = "navbar-item linkdiv" href="login.html">Login</a>
+</div>
         <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -27,18 +32,18 @@ class Header extends HTMLElement {
                 <a class="hvr" href="produtos.html" >Todas opções</a>
               </li><br>
               <li class="nav-item">
-                <a class="hvr"  href="#" >Aluguel em estúdio sem fotográfo</a>
+                <a class="hvr"  href="produtos.html" >Aluguel em estúdio sem fotográfo</a>
               </li><br>
               <li class="nav-item">
-                <a class="hvr"  href="#">Aluguel em estúdio com fotográfo</a>
+                <a class="hvr"  href="produtos.html">Aluguel em estúdio com fotográfo</a>
               </li><br>
               <li class="nav-item">
-                <a class="hvr"  href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="hvr"  href=""produtos.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Aluguel com retirada do aparelho
                 </a>
                 </li><br><br><br>
                 <li class="nav-item position-absolute bottom-0 ">
-                <a class="hvr"  href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false ">
+                <a class="hvr"  href="produtos.html" role="button" data-bs-toggle="dropdown" aria-expanded="false ">
                   Locais de retirada/estudios
                 </a><br><br>
                 </li>
